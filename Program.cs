1cace5eab182e7f9231f259dc344bf194db760f5
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-17 23:46:10 -0300

    Adiciona resolução ao exercício Sequência Pares ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SequenciaPares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nInicial;
+            int nInserido;
+
+            Console.Write("Digite um número natural positivo: ");
+
+            nInicial = 0;
+
+            nInserido = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Números Pares entre {nInicial} e {nInserido}.");
+
+            while(nInicial <= nInserido  )
+            {
+                if(nInicial % 2 == 0)
+                {
+                    Console.Write($" {nInicial} ");
+                }
+                nInicial = nInicial +1;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# SequenciaPares
-Exercício Sequência Pares ETEC 2021
+## Exercício `SequenciaPares` ETEC 2021
+
+Receba um número inteiro positivo. Exiba todos os números pares entre zero e o número digitado (inclusive).
+
+Ex.:
+```
+Números pares entre 0 e ? 15
+0 2 4 6 8 10 12 14
+```
